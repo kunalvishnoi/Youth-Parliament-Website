752fac690692bb9e0a20ad19f56a1dcf23a0a4a6
kunalvishnoi <vishnoi.kunal0@gmail.com>
2018-01-24 02:56:10 +0530

    about added

--- assets/scripts/script.js
+++ assets/scripts/script.js
@@ -1,8 +1,3 @@
-
-
-
-
-
 $(document).scroll(function(){
   $(".navbar").toggleClass("nav-small",$(this).scrollTop()>100);
   $(".logo-image").toggleClass("logo-image2",$(this).scrollTop()>100);
@@ -28,54 +23,27 @@ function smoothScroll(e){
         });
 }
 
-
+$("#contact-form").on('submit',function(e){
+  e.preventDefault();
+  const inputs = $("#contact-form :input");
+  let values={};
+  inputs.each(function() {
+    values[this.name] = this.value;
+  });
+  
+  $.post(' https://us-central1-rightcust-website.cloudfunctions.net/sendEmail',
+          values,
+          (data)=>{
+              console.log('done');
+              $("#contact-form").reset();
+          },
+          'application/json');
+});
 
 
 $("#myNavbar a").on('click',smoothScroll);
 $(".navbar-brand a").on('click',smoothScroll);
 
-
-function getTimeRemaining(endtime) {
-  var t = Date.parse(endtime) - Date.parse(new Date());
-  var seconds = Math.floor((t / 1000) % 60);
-  var minutes = Math.floor((t / 1000 / 60) % 60);
-  var hours = Math.floor((t / (1000 * 60 * 60)) % 24);
-  var days = Math.floor(t / (1000 * 60 * 60 * 24));
-  return {
-    'total': t,
-    'days': days,
-    'hours': hours,
-    'minutes': minutes,
-    'seconds': seconds
-  };
-}
-
-function initializeClock(id, endtime) {
-  var clock = document.getElementById(id);
-  var daysSpan = clock.querySelector('.days');
-  var hoursSpan = clock.querySelector('.hours');
-  var minutesSpan = clock.querySelector('.minutes');
-  var secondsSpan = clock.querySelector('.seconds');
-
-  function updateClock() {
-    var t = getTimeRemaining(endtime);
-
-    daysSpan.innerHTML = t.days;
-    hoursSpan.innerHTML = ('0' + t.hours).slice(-2);
-    minutesSpan.innerHTML = ('0' + t.minutes).slice(-2);
-    secondsSpan.innerHTML = ('0' + t.seconds).slice(-2);
-
-    if (t.total <= 0) {
-      clearInterval(timeinterval);
-    }
-  }
-
-  updateClock();
-  var timeinterval = setInterval(updateClock, 1000);
-}
-
-var deadline = new Date("february 17, 2018 09:00:00");
-initializeClock('clockdiv', deadline);
 
 
 
